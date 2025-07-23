873de0ab8c6beeb737c216be3fe76f54d55ffadc
giacomop1196 <giacomo.pillitteri65@gmail.com>
2025-07-23 15:27:40 +0200

    commit search

--- s9-l2/src/components/BookList.jsx
+++ s9-l2/src/components/BookList.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import SingleBook from "./SingleBook";
-import { Container, Row, Form, Button } from "react-bootstrap";
+import { Container, Row, Form } from "react-bootstrap";
 
 
 class BookList extends Component {
@@ -18,26 +18,33 @@ class BookList extends Component {
         //Lista dei libri in base al genere scelto in Welcome
         const currentGender = this.props.gender
 
+        const {searchForm} = this.state;
+
+        // Libri per titolo
+        // Converto in minuscolo per evitare errori
+        const filteredBooks = currentGender.filter((book) =>
+            book.title.toLowerCase().includes(searchForm.toLowerCase())
+        );
+
         return (
 
             <Container className="mt-2">
                 <Row>
 
                     <Form className="mb-3">
-                        <Form.Group className="mb-3 text-center" controlId="formBasicEmail">
+                        <Form.Group className="mb-3 text-center">
                             <Form.Label>Cerca per titolo</Form.Label>
                             <Form.Control type="text" placeholder="Titolo Libro" value={this.state.searchForm}
                                 onChange={this.searchChange} />
                         </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Cerca
-                        </Button>
                     </Form>
-
-                    {currentGender.map((book) => {
-                        return (
-                            <SingleBook book={book} key={book.asin} />)
-                    })}
+                    
+          {filteredBooks.length > 0 ? ( filteredBooks.map((book) => (
+              <SingleBook book={book} key={book.asin} />)))
+              :
+              (<p className="text-center text-danger mt-4">Nessun libro trovato con questo titolo.</p>
+          )}
+                   
                 </Row>
             </Container>
         )
